9ee6a77d3bffd014d36efc19a8888839c8365ec3
Kokonaut <gohgyul@gmail.com>
2014-05-08 02:36:10 -0700

    first try with exe setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+import sys
+from cx_Freeze import setup, Executable
+
+build_exe_options = {"packages":['Library', 'Graphs'], 
+					"includes":["tkinter", "csv", "subprocess", "datetime", "shutil", "random"],
+					"include_files": ['GUI','HTML','Users','Tests','Library','E.icns', 'Graphs'],
+					}
+
+base = None
+if sys.platform == "win32":
+	base = "Win32GUI"
+
+setup( name = "Excelerate",
+	version = "2.0"
+	description = "Excelerate Test Preparation"
+	options = {"build.exe": build_exe_options},
+	executables = [Executable("GUI.py", base=base)])
