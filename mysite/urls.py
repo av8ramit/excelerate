f6300f32782e5db09bf5b3f5cca82b278a732e5b
Kokonaut <gohgyul@gmail.com>
2014-07-28 21:15:38 -0700

    Removed userauth from url

--- mysite/urls.py
+++ mysite/urls.py
@@ -9,5 +9,5 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^userauth/', include('userauth.urls'))
+    url(r'^$', include('userauth.urls'))
 )
