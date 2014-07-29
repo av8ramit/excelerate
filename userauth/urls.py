4b59e01ce225fea54485d422b6d0e80a3e49baac
akittiudom <akittiudom@berkeley.edu>
2014-07-28 21:10:45 -0700

    fixed syntax in urls.py

--- userauth/urls.py
+++ userauth/urls.py
@@ -3,8 +3,8 @@ from django.conf.urls import patterns, url
 from userauth import views
 
 urlpatterns = patterns('', 
-		url(r'^$', views.home, name='home')
-		url(r'^/register/$', views.register, name='register')
-		url(r'^/login/$', views.login, name='login')
+		url(r'^$', 'views.home', name='home'),
+		url(r'^register/$', 'views.register', name='register'),
+		url(r'^login/$', 'views.login', name='login'),
 		)
 
