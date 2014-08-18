a947919c13595ce71d81ed65122ba70f7af6cff0
akittiudom <akittiudom@berkeley.edu>
2014-08-18 15:48:59 -0700

    added userpage and some more functionality

--- userauth/views.py
+++ userauth/views.py
@@ -33,16 +33,16 @@ def send(request):
 		email = request.POST.get('email')
 		studentid = request.POST.get('studentid')
 
-		if not p_word == re_pass:
-			error_message = "Passwords do not match"
-			context = #whatevs
-		regex = re.compile(".+?@.+?\..+")
-		if not regex.search(email):
-			error_message = "Incorrect Email Format"
-			context = #bitchs
-		if not studentid.isdigit():
-			error_message = "Please put a valid student id"
-			context = #peace
+		# if not p_word == re_pass:
+		# 	error_message = "Passwords do not match"
+		# 	context = #whatevs
+		# regex = re.compile(".+?@.+?\..+")
+		# if not regex.search(email):
+		# 	error_message = "Incorrect Email Format"
+		# 	context = #bitchs
+		# if not studentid.isdigit():
+		# 	error_message = "Please put a valid student id"
+		# 	context = #peace
 
 		user = Student.objects.create_user(username=u_name, password=p_word,
 									first_name=fname, last_name=lname,
@@ -60,7 +60,7 @@ def login(request):
 		p_word = request.POST.get('password')
 		# print u_name
 		# print p_word
-		#remember = request.POST.get('remember-me', False)
+		remember = request.POST.get('remember-me', False)
 		user = authenticate(username=u_name, password=p_word)
 		if user is not None:
 			# the password verified
@@ -68,7 +68,8 @@ def login(request):
 				# Change later
 				#login(request, user)
 				school = user.school_name
-				return HttpResponse(u_name + " from " + school + " succesfully logged in!")
+				# return HttpResponse(u_name + " from " + school + " succesfully logged in!")
+				return render(request, 'userauth/userpage.html', {'user':user})
 			else:
 				# User account has been disabled
 				return HttpResponse("Sorry, user has been disabled")
