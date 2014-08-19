8337850ae3c51831cc6d048079fda482361f3fdb
akittiudom <akittiudom@berkeley.edu>
2014-08-19 03:05:00 -0700

    added a quick home page

--- mysite/urls.py
+++ mysite/urls.py
@@ -9,7 +9,7 @@ urlpatterns = patterns('',
     # url(r'^$', 'mysite.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-	url(r'^$', 'userauth.views.home', name='home'),
+	url(r'^$', 'mysite.views.index', name='index'),
 	url(r'^login/', include('userauth.urls', namespace='login')),
     url(r'^admin/', include(admin.site.urls)),
 	
