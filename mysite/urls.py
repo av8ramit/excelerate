445515732f9aeed6761aeb95d5618174b996dd17
akittiudom <akittiudom@berkeley.edu>
2014-09-02 21:36:22 -0700

    added some stuff to home page to call excelerate functions

--- mysite/urls.py
+++ mysite/urls.py
@@ -12,5 +12,6 @@ urlpatterns = patterns('',
 	url(r'^$', 'mysite.views.index', name='index'),
 	url(r'^login/', include('userauth.urls', namespace='login')),
     url(r'^admin/', include(admin.site.urls)),
-	
+    url(r'^extest/$', 'mysite.views.extest', name='extest'),
+    url(r'^formtest/$', 'mysite.views.formtest', name='formtest'),
 )
--- mysite/views.py
+++ mysite/views.py
@@ -1,6 +1,54 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
+from Library.Console import *
+from Library.Values import *
+from django.core.context_processors import csrf
 
 
 def index(request):
 	return render(request, 'mysite/index_base.html')
+
+def extest(request):
+	"""
+	Test to see if we can call the console and new_class command from console.py in the Library
+	"""
+	# try:
+	# 	c = Console();
+	# 	val = c.process_commands("new_class Class")
+	# 	if val == False:
+	# 		response = c.error
+	# 	else:
+	# 		response = 'Successfully called excelerate function'
+	# 	return render(request, 'mysite/tester.html', {'response': response})
+	# except Exception as e:
+	# 	response = 'Something went wrong: '
+	# 	return render(request, 'mysite/tester.html', {'response':response, 'message':e})
+
+
+	c = Console();
+	val = c.process_commands("new_class Class")
+	if val == False:
+		response = c.error
+	else:
+		response = 'Successfully called excelerate function'
+	return render(request, 'mysite/tester.html', {'response': response})
+
+def formtest(request):
+	if request.POST:
+		context = {}
+		context.update(csrf(request))
+		cmd = request.POST.get('cmd')
+		try:
+			c = Console()
+			c.process_commands(cmd)
+			response = "Successfully called excelerate function: " + cmd
+			return render(request, 'mysite/tester.html', {'response':response})
+		except Exception as e:
+			response = 'Something went wrong with command:' + cmd + ". Exception outputted: "
+			return render(request, 'mysite/tester.html', {'response':response, 'message':e})
+	else:
+		response = 'Something wrong with post'
+		return render(request, 'mysite/tester.html', {'response':response})
+
+
+
