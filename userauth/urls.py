8e63a97cb1d775b2d67e0377409260d16aad1512
akittiudom <akittiudom@berkeley.edu>
2014-10-15 23:39:46 -0700

    added upload button and url

--- userauth/urls.py
+++ userauth/urls.py
@@ -8,6 +8,7 @@ urlpatterns = patterns('',
 		url(r'^auth/$', 'userauth.views.login_user', name='auth'),
 		url(r'^sendregister/$', 'userauth.views.send', name='send'),
 		url(r'^formtest2/$', 'userauth.views.formtest2', name='formtest2'),
+		url(r'^upload/$', 'userauth.views.upload_file', name='upload'),
 		# url(r'^sendregister/postregister/$', 'userauth.views.postregister', name='send_post'),
 		)
 
